1f76baabe1a3241e2fc6d89aad34f4e6ba8f2732
echizenyayota <virtualmisso30@gmail.com>
2022-01-31 16:51:48 +0900

    Resolving a mutation with errors

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -19,15 +19,21 @@ const resolvers = {
   Mutation: {
     // increments a track's numberOfViews property
     incrementTrackViews: async (_, { id }, { dataSources }) => {
-      const track = await dataSources.trackAPI.incrementTracksViews(id);
-
-      return {
-        code: 200,
-        success: true,
-        message: `Successfully incremented number of views for track ${id}`,
-        track,
+      try {
+        const track = await dataSources.trackAPI.incrementTracksViews(id);
+  
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        }; 
+      } catch (err) {
+        return {
+          
+        }
       }
-    }
+    },
   }, 
 
   Track: {
